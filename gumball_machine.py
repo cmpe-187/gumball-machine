66938a2893ab3a829518bcd439080b36d08bb4ad
Salleek <sbstevencai@gmail.com>
2020-02-19 13:58:18 -0800

    Removed convert_currency function and changed insert function

--- gumball_machine.py
+++ gumball_machine.py
@@ -1,23 +1,28 @@
 class gumball_machine:
     def __init__(self):
         self.valid_currency = {"nickel": 5, "dime": 10, "quarter": 25}
-        self.money_inserted = []
         self.money_value = 0
         self.money_returned = []
 
     def insert(self, money):
-        self.money_inserted.append(money)
-
-    def convert_currency(self):
-        for x in range(len(self.money_inserted) - 1):
-            if self.money_inserted[x] in self.valid_currency:
-                self.money_value += self.valid_currency[self.money_inserted[x]]
-            else:
-                self.money_returned.append(self.money_inserted[x])
+        if money in self.valid_currency:
+            self.money_value += self.valid_currency[money]
+        else:
+            self.money_returned.append(money)
 
     def dispense_red(self):
-        if self.money_value == 0:
-            self.convert_currency()
+        if self.money_value >= 5:
+            self.money_value -= 5
+        else:
+            print("You need at least 5 cents to dispense a red gumball\n")
+
+    def dispense_yellow(self):
+        if self.money_value >= 10:
+            self.money_value -= 10
+        else:
+            print("You need at least 10 cents to dispense a red gumball\n")
+
+
 
 
 
