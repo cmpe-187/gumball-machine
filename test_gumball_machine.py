2f9d4b8591e49835f99cb52651353fc00fd4e4f6
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-02-20 20:27:23 -0800

    Refactor test_gumball_machine
    Explicitly state input vectors, expected outputs

--- test_gumball_machine.py
+++ test_gumball_machine.py
@@ -6,16 +6,25 @@ class NoMoneyTestCases(unittest.TestCase):
     gumball_machine = gumball_machine_class.GumballMachine()
 
     def test_dispense_red(self):
-        response = self.gumball_machine.dispense_red()
-        self.assertEqual(response, "You need at least 5 cents to dispense a red gumball")
+        # input: dispense_red()
+        expected_output = "You need at least 5 cents to dispense a red gumball"
+
+        actual_output = self.gumball_machine.dispense_red()
+        self.assertEqual(expected_output, actual_output)
 
     def test_dispense_yellow(self):
-        response = self.gumball_machine.dispense_yellow()
-        self.assertEqual("You need at least 10 cents to dispense a yellow gumball", response)
+        # input: dispense_yellow()
+        expected_output = "You need at least 10 cents to dispense a yellow gumball"
+
+        actual_output = self.gumball_machine.dispense_yellow()
+        self.assertEqual(expected_output, actual_output)
 
     def test_return_my_change(self):
-        response = self.gumball_machine.return_my_change()
-        self.assertEqual("There is no change to return", response)
+        # input: return_my_change()
+        expected_output = "There is no change to return"
+
+        actual_output = self.gumball_machine.return_my_change()
+        self.assertEqual(expected_output, actual_output)
 
 
 if __name__ == '__main__':
