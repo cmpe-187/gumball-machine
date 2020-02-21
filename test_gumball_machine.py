872e5bcd5affe43da095416fbcb1d783893723c7
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-02-20 20:59:42 -0800

    Add initial "exact money" test cases

--- test_gumball_machine.py
+++ test_gumball_machine.py
@@ -30,5 +30,37 @@ class NoMoneyTestCases(unittest.TestCase):
         self.assertEqual(expected_output, actual_output)
 
 
+class ExactMoneyTestCases(unittest.TestCase):
+    gumball_machine = gumball_machine_class.GumballMachine()
+
+    def test_insert_nickel_dispense_red(self):
+        """Tests dispensing a red gumball with a nickel (5 cents) in the machine"""
+        # input: insert("nickel"), dispense_red()
+        expected_output = "Enjoy your red gumball"
+
+        self.gumball_machine.insert("nickel")
+        actual_output = self.gumball_machine.dispense_red()
+        self.assertEqual(expected_output, actual_output)
+
+    def test_insert_dime_dispense_yellow(self):
+        """Tests dispensing a yellow gumball with a dime (10 cents) in the machine"""
+        # input: insert("dime"), dispense_yellow()
+        expected_output = "Enjoy your yellow gumball"
+
+        self.gumball_machine.insert("dime")
+        actual_output = self.gumball_machine.dispense_yellow()
+        self.assertEqual(expected_output, actual_output)
+
+    def test_insert_nickels_dispense_yellow(self):
+        """Tests dispensing a yellow gumball with two nickels (10 cents) in the machine"""
+        # input: insert("nickel"), insert("nickel"), dispense_yellow()
+        expected_output = "Enjoy your yellow gumball"
+
+        self.gumball_machine.insert("nickel")
+        self.gumball_machine.insert("nickel")
+        actual_output = self.gumball_machine.dispense_yellow()
+        self.assertEqual(expected_output, actual_output)
+
+
 if __name__ == '__main__':
     unittest.main()
