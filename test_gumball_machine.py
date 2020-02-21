3acb07f293c89a88a0d9aff25de12c09adf0bbaf
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-02-20 20:46:57 -0800

    Add docstrings to test functions

--- test_gumball_machine.py
+++ test_gumball_machine.py
@@ -6,6 +6,7 @@ class NoMoneyTestCases(unittest.TestCase):
     gumball_machine = gumball_machine_class.GumballMachine()
 
     def test_dispense_red(self):
+        """Tests dispensing a red gumball with no money in the machine"""
         # input: dispense_red()
         expected_output = "You need at least 5 cents to dispense a red gumball"
 
@@ -13,6 +14,7 @@ class NoMoneyTestCases(unittest.TestCase):
         self.assertEqual(expected_output, actual_output)
 
     def test_dispense_yellow(self):
+        """Tests dispensing a yellow gumball with no money in the machine"""
         # input: dispense_yellow()
         expected_output = "You need at least 10 cents to dispense a yellow gumball"
 
@@ -20,6 +22,7 @@ class NoMoneyTestCases(unittest.TestCase):
         self.assertEqual(expected_output, actual_output)
 
     def test_return_my_change(self):
+        """Tests returning change with no money in the machine"""
         # input: return_my_change()
         expected_output = "There is no change to return"
 
